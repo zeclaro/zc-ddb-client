405cf482a6c01c79909e38f913e7bfe962ec1c69
Jose Machado <zeclaro@gmail.com>
2021-06-06 00:30:43 +0100

    Changed the way to save the value. It's now a string. Updated/improved tests.

--- src/zcddbclient/ddb_client.py
+++ src/zcddbclient/ddb_client.py
@@ -7,7 +7,6 @@ import logging
 from .env_setup import DDB_TABLE_NAME
 from datetime import datetime
 from .constants import UUID_GENERATION_ORIGIN_DATE, TIME_STAMP_FORMAT
-
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(DDB_TABLE_NAME)
 
@@ -19,10 +18,14 @@ def _generate_uuid(device_id: str):
     return f"#{device_id}#{int(microseconds)}"
 
 
-def write_item(*, qualifier: str, value: float, unit: str, device_id: str, location: str):
+def write_item(*, qualifier: str, value: str, unit: str, device_id: str, location: str):
     item_uuid = _generate_uuid(device_id)
     logging.debug(f"Writing item {item_uuid} in table {DDB_TABLE_NAME}")
     now = datetime.now()
+
+    check_float = isinstance(float(value), float)
+    assert check_float is True
+
     item = dict()
 
     item["PK"] = item_uuid
--- tests/test_ddb_client.py
+++ tests/test_ddb_client.py
@@ -1,5 +1,6 @@
 import unittest
 from zcddbclient import ddb_client
+from decimal import Decimal
 
 
 class DdbClientTests(unittest.TestCase):
@@ -10,11 +11,31 @@ class DdbClientTests(unittest.TestCase):
     #   add more tests
     #   automate testing with Tox
 
-    def test_create_new_item_in_table(self):
+    def test_create_item(self):
         ddb_client.write_item(
             qualifier="speed_test",
             device_id="rasp_pi",
             location="Home",
             value="100",
+            unit="Mbps"
+        )
+
+    def test_create_item_float_value(self):
+        ddb_client.write_item(
+            qualifier="speed_test_2",
+            device_id="rasp_pi",
+            location="Home",
+            value="10.984598",
+            unit="Mbps"
+        )
+
+    def test_create_item_invalid_value(self):
+        self.assertRaises(
+            ValueError,
+            ddb_client.write_item,
+            qualifier="speed_test_2",
+            device_id="rasp_pi",
+            location="Home",
+            value="this is not a number",
             unit="Mbps"
         )
